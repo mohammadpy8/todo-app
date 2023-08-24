8f2a647f53d836a0473f1bf12ffb977880a95f99
mohammadpy8 <mohammadpy8@gmail.com>
2023-08-24 13:34:17 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Test from './Test'
+
+import './App.css'
+
+const App = () :React.ReactNode => {
+  return (
+    <>
+      <Test />
+      <h1>mohammad</h1>
+    </>
+  )
+}
+
+export default App
