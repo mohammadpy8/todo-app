58a68f6aaebb8067c217c694b5efac29c3bf30a6
mohammadpy8 <mohammadpy8@gmail.com>
2023-08-24 13:39:58 +0330

    components-type

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import Test from './Test'
 
 import './App.css'
 
-const App = () :React.ReactNode => {
+const App: React.FC = (): React.ReactNode => {
   return (
     <>
       <Test />
       <h1>mohammad</h1>
     </>
   )
-}
+};
 
 export default App
