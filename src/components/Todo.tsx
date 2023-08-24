69cc69c63f9f912ba3aa4e68530cc6ea59a58e82
mohammadpy8 <mohammadpy8@gmail.com>
2023-08-24 18:31:30 +0330

    event

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -7,11 +7,20 @@ const Todo: React.FC<React.PropsWithChildren<TodoType>> = ({ id, title, isComplt
     const styles: React.CSSProperties = {
         fontSize: "3.5rem",
         backgroundColor: color,
-    }
+    };
+
+    const clickHandler = (event: React.MouseEvent<HTMLHeadingElement, MouseEvent>) => event.preventDefault();
+
+    const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => event.preventDefault();
+
+    const submitHandler = (event: React.FormEvent<HTMLFormElement>) => event.preventDefault();
 
     return (
         <ul style={styles}>
-            <h2>title: {title}</h2>
+            <h2 onClick={clickHandler}>title: {title}</h2>
+            <form action="#" onSubmit={submitHandler}>
+                <input type="text" onChange={changeHandler} />
+            </form>
         </ul>
     );
 };
